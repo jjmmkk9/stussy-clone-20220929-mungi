50cd41885044c86df6f5b7eacbc3812252bd14c1
jjmmkk9 <abcdqwerty0192@gmail.com>
2022-09-29 09:24:47 +0900

    syussy 사이트 클론 패키지

---
+++ src/main/java/com/demo/StussyClone20220929MungiApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220929MungiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StussyClone20220929MungiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/StussyClone20220929MungiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220929MungiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
